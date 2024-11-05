3bc44e4d46e65ed5111e1120aeaab153d68dfac7
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2024-11-04 19:39:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -102,10 +102,7 @@ SplomCell.prototype.update = function(g, data) {
         .attr('cy', function(d){
             return yScale(d[_this.y]);
         })
-        .style('fill', function(d) { 
-		colorAttribute = d3.scaleSequential(d3["interpolate" + buttonValue])
-        .domain([0, width]);
-		return colorScale(d[colorAttribute]); });
+        .style('fill', function(d) { return colorScale(d[colorAttribute]); });
 
 
     dots.exit().remove();
